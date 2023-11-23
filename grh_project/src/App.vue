1d6caefbc195c0adcdf48d022bfb4e7be99929d1
Poonam Gahatraj <poonamgtrz@gmail.com>
2023-11-23 18:32:22 +0545

    grh_project

---
+++ grh_project/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+import product_Info from './components/product_Info.vue';
+</script>
+
+<template>
+  
+<product_Info/>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ grh_project/src/components/product_Info.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1 class="text-4xl flex justify-center">Create a Product</h1>
+    <form>
+        <label class="font-semibold">Product name</label><br>
+        <input type="text" placeholder="Pixel 6 Pro"><br>
+        <hr>
+        <label class="font-semibold">Product family name</label><br>
+        <input type="text" placeholder="Pixel"><br>
+        <hr>
+        <label class="font-semibold">Select product from Inventory</label><br>
+       <button class="bg-sky-500 text-gray-50 w-24 rounded">Select</button>
+        <hr>
+        <label class="font-semibold">Year Product Released</label><br>
+        <input type="text" placeholder="Enter the year product released here"><br>
+        <hr>
+        <label class="font-semibold">Type of Product</label><br>
+        <input type="text" placeholder="Choose the type of product"><br>
+        <hr>
+
+    </form>
+</template>
